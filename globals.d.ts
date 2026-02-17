f054b29efa0355aaba949480a5adb6781276d383
Mnem42 <Mnem424242@gmail.com>
2026-02-17 19:16:18 +0000

    fix: proper typing for generateSave's pixelMap arg

--- globals.d.ts
+++ globals.d.ts
@@ -879,7 +879,7 @@ type NormalSave = {
  * The generated save. {@link NormalSave} if `raw` is `false` and {@link RawDataSave} otherwise
  */
 declare function generateSave(
-    pixelMap?: Pixel[][],
+    pixelMap?: (Pixel | undefined)[][],
     options?: {
         raw?: true
         mods?: boolean
